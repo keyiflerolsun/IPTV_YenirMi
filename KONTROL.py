d615e6af07b6aa93d5d4469fa213f7e0d300b1d3
keyiflerolsun <keyiflerolsun@gmail.com>
2024-07-19 14:12:05 +0300

    🕊
    🕊

--- KONTROL.py
+++ KONTROL.py
@@ -6,91 +6,97 @@ from cloudscraper import CloudScraper
 from re           import compile, MULTILINE
 from os           import remove
 
-def iptv_parser(dosya_yolu:str) -> list[dict]:
-    with open(dosya_yolu, "r", encoding="utf-8") as dosya:
-        satirlar = dosya.readlines()
+class IPTVParser:
+    def __init__(self, dosya_yolu: str):
+        self.dosya_yolu   = dosya_yolu
+        self.kanallar     = []
+        self.hata_bulundu = False
 
-    extinf_re     = compile(r'#EXTINF:-1 tvg-name="([^"]+)"')
-    url_re        = compile(r'^(https?://[^\s]+)', MULTILINE)
-    user_agent_re = compile(r'#EXTVLCOPT:http-user-agent=(.*)')
-    referer_re    = compile(r'#EXTVLCOPT:http-referrer=(.*)')
+    def dosya_parse(self):
+        with open(self.dosya_yolu, "r", encoding="utf-8") as dosya:
+            satirlar = dosya.readlines()
 
-    kanallar     = []
-    mevcut_kanal = {}
+        extinf_re     = compile(r'#EXTINF:-1 tvg-name="([^"]+)"')
+        url_re        = compile(r'^(https?://[^\s]+)', MULTILINE)
+        user_agent_re = compile(r'#EXTVLCOPT:http-user-agent=(.*)')
+        referer_re    = compile(r'#EXTVLCOPT:http-referrer=(.*)')
 
-    for satir in satirlar:
-        if satir.startswith("#EXTINF"):
-            if match := extinf_re.search(satir):
-                if mevcut_kanal:
-                    kanallar.append(mevcut_kanal)
+        mevcut_kanal  = {}
 
-                mevcut_kanal = {"ad": match[1], "user-agent": None, "referer": None}
+        for satir in satirlar:
+            if satir.startswith("#EXTINF"):
+                if match := extinf_re.search(satir):
+                    if mevcut_kanal:
+                        self.kanallar.append(mevcut_kanal)
 
-        elif satir.startswith("#EXTVLCOPT:http-user-agent"):
-            if match := user_agent_re.search(satir):
-                mevcut_kanal["user-agent"] = match[1]
+                    mevcut_kanal = {"ad": match[1], "user-agent": None, "referer": None}
 
-        elif satir.startswith("#EXTVLCOPT:http-referrer"):
-            if match := referer_re.search(satir):
-                mevcut_kanal["referer"] = match[1]
+            elif satir.startswith("#EXTVLCOPT:http-user-agent"):
+                if match := user_agent_re.search(satir):
+                    mevcut_kanal["user-agent"] = match[1]
 
-        elif satir.startswith("http"):
-            if match := url_re.search(satir):
-                mevcut_kanal["yayin"] = match[0]
-                kanallar.append(mevcut_kanal)
-                mevcut_kanal = {}
+            elif satir.startswith("#EXTVLCOPT:http-referrer"):
+                if match := referer_re.search(satir):
+                    mevcut_kanal["referer"] = match[1]
 
-    if mevcut_kanal:
-        kanallar.append(mevcut_kanal)
+            elif satir.startswith("http"):
+                if match := url_re.search(satir):
+                    mevcut_kanal["yayin"] = match[0]
+                    self.kanallar.append(mevcut_kanal)
+                    mevcut_kanal = {}
 
-    return kanallar
+        if mevcut_kanal:
+            self.kanallar.append(mevcut_kanal)
 
-kanallar     = iptv_parser("Kanallar/KekikAkademi.m3u")
-hata_bulundu = False
+    def kanallar_kontrol(self):
+        satir_ekle("HATALAR.md", """
+        ***
 
-satir_ekle("HATALAR.md", """
+        > # [![Yayın Kontrolü](https://github.com/keyiflerolsun/IPTV_YenirMi/actions/workflows/Kontrol.yml/badge.svg)](https://github.com/keyiflerolsun/IPTV_YenirMi/actions/workflows/Kontrol.yml)
+        > ### [Kanallar/KekikAkademi.m3u](https://github.com/keyiflerolsun/IPTV_YenirMi/blob/main/Kanallar/KekikAkademi.m3u)
 
-***
+        ***
 
-> # [![Yayın Kontrolü](https://github.com/keyiflerolsun/IPTV_YenirMi/actions/workflows/Kontrol.yml/badge.svg)](https://github.com/keyiflerolsun/IPTV_YenirMi/actions/workflows/Kontrol.yml)
-> ### [Kanallar/KekikAkademi.m3u](https://github.com/keyiflerolsun/IPTV_YenirMi/blob/main/Kanallar/KekikAkademi.m3u)
+        | AD | HATA | YAYIN |
+        |----|------|-------|
+        """.strip())
 
-***
+        for kanal in self.kanallar:
+            print("\n")
+            konsol.log(f"[~] Kontrol Ediliyor : {kanal['ad']}")
 
-| AD | HATA | YAYIN |
-|----|------|-------|
+            oturum = CloudScraper()
 
-""".strip())
+            if kanal["user-agent"]:
+                oturum.headers.update({"User-Agent": kanal["user-agent"]})
 
-for kanal in kanallar:
-    print("\n")
-    konsol.log(f"[~] Kontrol Ediliyor : {kanal['ad']}")
+            if kanal["referer"]:
+                oturum.headers.update({"Referer": kanal["referer"]})
 
-    oturum = CloudScraper()
+            try:
+                istek = oturum.get(kanal["yayin"], timeout=5)
+            except Exception as hata:
+                konsol.log(f"[!] {type(hata).__name__} : {hata}")
+                satir_ekle("HATALAR.md", f"|**{kanal['ad']}**|`{type(hata).__name__}`|*{kanal['yayin']}*|")
+                self.hata_bulundu = True
+                continue
 
-    if kanal["user-agent"]:
-        oturum.headers.update({"User-Agent": kanal["user-agent"]})
+            if istek.status_code == 200:
+                konsol.log(f"[+] Kontrol Edildi   : {kanal['ad']}")
+            else:
+                konsol.log(f"[!] {istek.status_code} » {kanal['yayin']} » {kanal['ad']}")
+                satir_ekle("HATALAR.md", f"|**{kanal['ad']}**|`{istek.status_code}`|*{kanal['yayin']}*|")
+                self.hata_bulundu = True
 
-    if kanal["referer"]:
-        oturum.headers.update({"Referer": kanal["referer"]})
+        if not self.hata_bulundu:
+            remove("HATALAR.md")
+            print("\n")
+            konsol.log("[+] Hata Bulunamadı.")
 
-    try:
-        istek = oturum.get(kanal['yayin'], verify=False, timeout=5)
-    except Exception as hata:
-        konsol.log(f"[!] {type(hata).__name__} : {hata}")
-        satir_ekle("HATALAR.md", f"|**{kanal['ad']}**|`{type(hata).__name__}`|*{kanal['yayin']}*|")
-        hata_bulundu = True
-        continue
+    def run(self):
+        self.dosya_parse()
+        self.kanallar_kontrol()
 
-    if istek.status_code == 200:
-        konsol.log(f"[+] Kontrol Edildi   : {kanal['ad']}")
-    else:
-        konsol.log(f"[!] {istek.status_code} » {kanal['yayin']} » {kanal['ad']}")
-        satir_ekle("HATALAR.md", f"|**{kanal['ad']}**|`{istek.status_code}`|*{kanal['yayin']}*|")
-        hata_bulundu = True
-
-
-if not hata_bulundu:
-    remove("HATALAR.md")
-    print("\n")
-    konsol.log("[+] Hata Bulunamadı.")
+if __name__ == "__main__":
+    parser = IPTVParser("Kanallar/KekikAkademi.m3u")
+    parser.run()
