d27854f0373fbca5870efb2de7e8be5e01d5847e
keyiflerolsun <keyiflerolsun@gmail.com>
2024-10-25 15:03:11 +0300

    🕊
    🕊

--- TRGoals.py
+++ TRGoals.py
@@ -47,7 +47,7 @@ class TRGoals:
             yeni_domain = self.trgoals_domaini_al()
         except Exception:
             try:
-                yeni_domain = self.redirect_gec("https://bit.ly/3Byg5Iw")
+                yeni_domain = self.redirect_gec("https://bit.ly/3Byg5Iw?r=lp&m=Mo35cuwH2jM")
             except Exception:
                 try:
                     yeni_domain = self.redirect_gec(eldeki_domain)
