886c2185812a5898661c53e44c7edb78a4ac37f2
keyiflerolsun <keyiflerolsun@gmail.com>
2025-02-20 19:40:35 +0300

    🕊
    🕊

--- TRGoals.py
+++ TRGoals.py
@@ -87,7 +87,7 @@ class TRGoals:
         eski_yayin_url = eski_yayin_url[0]
         konsol.log(f"[yellow][~] Eski Yayın URL : {eski_yayin_url}")
 
-        kimlik = self.httpx.post("http://10.0.2.0:1221/api/v1/kimlik", json={"url": yeni_domain}).json()
+        kimlik   = self.httpx.post("http://10.0.2.0:1221/api/v1/kimlik", json={"url": yeni_domain}).json()
         self.httpx.cookies.update(kimlik["cookies"])
         self.httpx.headers.update(kimlik["headers"])
         response = self.httpx.get(kontrol_url)
